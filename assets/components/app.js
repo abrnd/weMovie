cdd4fc222e2b9ea9db99ef11c8f7f0d61414107a
abrnd <antoine.bernard@live.fr>
2022-10-31 20:05:03 +0100

    init webpack et react

---
+++ assets/components/app.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import axios from 'axios';
+
+const App = () => {
+
+    
+    return(
+        <div>
+            hello!
+        </div>
+    )
+}
+
+
+export default App;
